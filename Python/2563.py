55ef0448ddf6043bfa82e4d350aa16c5eca0f442
park min kyu <88785472+parkmingue00@users.noreply.github.com>
2023-07-14 15:48:31 +0900

    Add algorithm source code

---
+++ Python/2563.py
@@ -0,0 +1,20 @@
+import sys
+
+num = int(sys.stdin.readline())
+
+space = [[0 for _ in range(100)] for _ in range(100)]
+
+for i in range(num):
+    x, y = map(int, sys.stdin.readline().split())
+    for j in range(x, x+10):
+        for k in range(y, y+10):
+            space[k][j] = 1
+
+count = 0
+
+for i in range(100):
+    for j in range(100):
+        if space[i][j] == 1:
+            count += 1
+
+print(count)   
