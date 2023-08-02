de9ebc1e1b418d2991a70eddbe3eae906a2ae028
Mingyu Park <88785472+parkm2ngyu00@users.noreply.github.com>
2023-08-03 00:14:55 +0900

    [Silver III] Title: 퇴사, Time: 48 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -0,0 +1,15 @@
+import sys
+
+n = int(input())
+
+schedule = [list(map(int, sys.stdin.readline().split())) for i in range(n)]
+
+dp = [0 for i in range(n+1)]
+
+for i in range(n-1, -1, -1):
+    if i + schedule[i][0] > n:
+        dp[i] = dp[i+1]
+    else:
+        dp[i] = max(dp[i+1], dp[i + schedule[i][0]] + schedule[i][1])
+
+print(dp[0])
