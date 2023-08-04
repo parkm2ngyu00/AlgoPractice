651dbfcc9104844f53b660ae8eb6fd87a612147a
Mingyu Park <88785472+parkm2ngyu00@users.noreply.github.com>
2023-08-04 23:24:15 +0900

    [Silver II] Title: 유기농 배추, Time: 60 ms, Memory: 31652 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,40 +1,28 @@
-from collections import deque
+import sys
+sys.setrecursionlimit(10000)
 
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
-
-def bfs(x, y):
-    queue = deque()
-    queue.append((x, y))
-    while len(queue) > 0:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx < 0 or ny < 0 or nx >= N or ny >= M:
-                continue
-            if matrix[nx][ny] == 1:
-                queue.append((nx, ny))
-                matrix[nx][ny] = 0 
-    return
-
-T = int(input())
-res_list = []
-
-for _ in range(T):
-    M, N, K = map(int, input().split())
-    matrix = [[0 for _ in range(M)] for _ in range(N)]
-    for i in range(K):
-        x, y = map(int, input().split())
-        matrix[y][x] = 1
+def dfs(x, y):
+    dx = [0, 0, -1, 1]
+    dy = [1, -1, 0, 0]
+    
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if (0 <= nx < n) and (0 <= ny < m):
+            if field[nx][ny] == 1:
+                field[nx][ny] = 0
+                dfs(nx, ny)
+                
+for _ in range(int(sys.stdin.readline())):
+    m, n, k = map(int, sys.stdin.readline().split())
+    field = [[0 for _ in range(m)] for _ in range(n)]
     count = 0
-    for i in range(N):
-        for j in range(M):
-            if matrix[i][j] == 1:
-                matrix[i][j] = 0
-                bfs(i, j)
+    for _ in range(k):
+        x, y = map(int, sys.stdin.readline().split())
+        field[y][x] = 1
+    for x in range(n):
+        for y in range(m):
+            if field[x][y] == 1:
+                dfs(x, y)
                 count += 1
-    res_list.append(count)
-
-for i in res_list:
-    print(i)
+    print(count)
